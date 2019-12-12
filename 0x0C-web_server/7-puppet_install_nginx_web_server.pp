a2432d9ddcc73886e5fb3b50e221f854a0be249e
JUCORTESA <984@holbertonschool.com>
2019-12-12 03:02:44 +0000

    [Add] puppet

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,10 @@
+# Install Nginx web server using Puppet
+
+exec {'nginx_install':
+  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Holberton School" | sudo tee /var/www/html/index.html',
+  provider => shell,
+}
+exec {'nginx_full':
+  command  => sudo sed -i "30i \\\tlocation /redirect_me {\n\t\treturn 301 https://www.youtube.com/watch?v=oavMtUWDBTM;\n}" /etc/nginx/sites-available/default ; sudo service nginx restart',
+  provider => shell,
+}
