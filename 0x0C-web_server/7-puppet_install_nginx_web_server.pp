9186277a186becb6af17711e2228f88fd6799489
JUCORTESA <984@holbertonschool.com>
2019-12-12 03:09:22 +0000

    [fix] missing '

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -5,6 +5,6 @@ exec {'nginx_install':
   provider => shell,
 }
 exec {'nginx_full':
-  command  => sudo sed -i "30i \\\tlocation /redirect_me {\n\t\treturn 301 https://www.youtube.com/watch?v=oavMtUWDBTM;\n}" /etc/nginx/sites-available/default ; sudo service nginx restart',
+  command  => 'sudo sed -i "30i \\\tlocation /redirect_me {\n\t\treturn 301 https://www.youtube.com/watch?v=oavMtUWDBTM;\n}" /etc/nginx/sites-available/default ; sudo service nginx restart',
   provider => shell,
 }
