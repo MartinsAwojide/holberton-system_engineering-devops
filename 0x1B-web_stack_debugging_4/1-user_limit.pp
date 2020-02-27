dec26ac6fa50f0e651dfcfbc57b684ddbf81da61
jucortesa <984@holbertonschool.com>
2020-02-27 14:27:20 -0500

    [add] holberton user

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,13 @@
+# Script that fix the limits for user holberton
+exec { 'Correct hard':
+  command  => 'sudo sed -i \'s/nofile 5/nofile 30000/\' /etc/security/limits.conf',
+  provider => shell,
+}
+exec { 'Correct soft':
+  command  => 'sudo sed -i \'s/nofile 4/nofile 10000/\' /etc/security/limits.conf',
+  provider => shell,
+}
+exec { 'restart':
+  command  => 'sysctl -p',
+  provider => shell,
+}
